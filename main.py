21d8c5b5593ce953aa55a132cb2619bb4f477c1d
wilson <3050166231@qq.com>
2022-11-05 17:59:27 +0800

    pool 5 -> 2 release size 30 -> 10

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from multiprocessing import Pool
 
 if __name__ == '__main__':
     releases_json = get_releases_json(
-        "https://api.github.com/repos/CleverRaven/Cataclysm-DDA/releases?per_page=30")
+        "https://api.github.com/repos/CleverRaven/Cataclysm-DDA/releases?per_page=10")
     exist_files = get_path_files(download_dir)
     down_urls, remove_files = get_all_need_download_game_file_dirt(
         releases_json, exist_files)
     print(f"down size is {len(down_urls)}")
-    pool = Pool(5)
+    pool = Pool(2)
     for i in down_urls:
         pool.apply_async(download_game_file, args=(i,))
     pool.close()
